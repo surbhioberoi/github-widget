8dc1bc1d9a5814bed7dbedcdf02360db10f82bc3
Surbhi Oberoi <siyaoberoi93@gmail.com>
2016-07-31 13:37:26 +0530

    removing JQuery dependency

--- src/widget.js
+++ src/widget.js
@@ -1,15 +1,49 @@
-$(document).ready(function() {
-    $("body").append("<style>@import url(https://fonts.googleapis.com/css?family=Noto+Sans:400,700);.gh-widget-link,.gh-widget-link:hover{text-decoration:none}.gh-widget-container{display:flex;flex-direction:row;flex-wrap:no-wrap;align-items:center;justify-content:center;color:#333;font-family:'Noto Sans',sans-serif}.gh-widget-personal-details .bio,.gh-widget-stats .count{color:#4078C0}.github-widget{border:1px solid #DDD;max-width:350px}.gh-widget-item{flex:1;text-align:center;padding:10px}.gh-widget-repositories .language{text-align:left}.gh-widget-repositories .language div,.gh-widget-repositories .stars div{padding:5px 0}.gh-widget-photo{flex:2}.gh-widget-photo img{border-radius:100%;max-width:90px}.gh-widget-personal-details{flex:6}.gh-widget-personal-details .full-name{font-size:1.5em;line-height:1.5em}.gh-widget-personal-details .location{font-size:.8em}.gh-widget-stats .count{font-size:1.2em;font-weight:700}.gh-widget-repositories .names{flex:2;text-align:left}.gh-widget-repositories .names div{padding:5px 0;text-overflow:ellipsis}.gh-widget-follow{flex:2}.gh-widget-active-time{flex:4;font-size:.8em}.gh-widget-heading{font-weight:400;color:#666}.gh-widget-hr{border:1px solid #DDD}.gh-widget-link{color:#4078C0}.gh-widget-follow button{width:100%;height:2em;border:none;background:#ddd}</style>");
-    $(".github-widget").append('<div class="gh-widget-container"><div class="gh-widget-item gh-widget-photo"></div><div class="gh-widget-item gh-widget-personal-details"></div></div><div class="gh-widget-container gh-widget-stats"></div><hr class="gh-widget-hr"><div class="gh-widget-container"><div class="gh-widget-item gh-widget-heading">Top repositories</div></div><div class="gh-widget-repositories"></div><div class="gh-widget-container"><div class="gh-widget-item gh-widget-follow"></div><div class="gh-widget-item gh-widget-active-time"></div></div>');
 
-    var username = $(".github-widget").data("username");
+function appendToWidget(parentSelector, tag, classes, html) {
+    var parentNode = document.querySelector(parentSelector);
+    var childNode = document.createElement(tag);
+    childNode.innerHTML = html;
+    childNode.className += classes;
+    parentNode.appendChild(childNode);
+}
+
+function getJSON(url, callback) {
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.onload = function() {
+      if (request.status === 200) {
+        var data = JSON.parse(request.responseText);
+        callback(data);
+      }
+    };
+    request.send();
+}
+
+
+function ready(fn) {
+  if (document.readyState != 'loading'){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+
+
+function start() {
+    appendToWidget("body", "style", "", "@import url(https://fonts.googleapis.com/css?family=Noto+Sans:400,700);.gh-widget-link,.gh-widget-link:hover{text-decoration:none}.gh-widget-container{display:flex;flex-direction:row;flex-wrap:no-wrap;align-items:center;justify-content:center;color:#333;font-family:'Noto Sans',sans-serif}.gh-widget-personal-details .bio,.gh-widget-stats .count{color:#4078C0}.github-widget{border:1px solid #DDD;max-width:350px}.gh-widget-item{flex:1;text-align:center;padding:10px}.gh-widget-repositories .language{text-align:left}.gh-widget-repositories .language div,.gh-widget-repositories .stars div{padding:5px 0}.gh-widget-photo{flex:2}.gh-widget-photo img{border-radius:100%;max-width:90px}.gh-widget-personal-details{flex:6}.gh-widget-personal-details .full-name{font-size:1.5em;line-height:1.5em}.gh-widget-personal-details .location{font-size:.8em}.gh-widget-stats .count{font-size:1.2em;font-weight:700}.gh-widget-repositories .names{flex:2;text-align:left}.gh-widget-repositories .names div{padding:5px 0;text-overflow:ellipsis}.gh-widget-follow{flex:2}.gh-widget-active-time{flex:4;font-size:.8em}.gh-widget-heading{font-weight:400;color:#666}.gh-widget-hr{border:1px solid #DDD}.gh-widget-link{color:#4078C0}.gh-widget-follow button{width:100%;height:2em;border:none;background:#ddd}");
+    appendToWidget(".github-widget", "div", "", '<div class="gh-widget-container"><div class="gh-widget-item gh-widget-photo"></div><div class="gh-widget-item gh-widget-personal-details"></div></div><div class="gh-widget-container gh-widget-stats"></div><hr class="gh-widget-hr"><div class="gh-widget-container"><div class="gh-widget-item gh-widget-heading">Top repositories</div></div><div class="gh-widget-repositories"></div><div class="gh-widget-container"><div class="gh-widget-item gh-widget-follow"></div><div class="gh-widget-item gh-widget-active-time"></div></div>')
+    var username = document.querySelector(".github-widget").dataset.username;
     fetchRepos(username);
     fetchUserDetails(username);
-})
+}
+
+ready(start);
+
 
 function fetchRepos(username) {
     var url = "https://api.github.com/users/" + username + "/repos";
-    $.get(url, function(response) {
+    getJSON(url, function(response) {
         updateRepoDetails(topRepos(response));
         updateLastPush(lastPushedDay(response));
     });
@@ -17,13 +51,13 @@ function fetchRepos(username) {
 
 function fetchUserDetails(username) {
     var url = "https://api.github.com/users/" + username;
-    $.get(url, function(response) {
+    getJSON(url, function(response) {
         updateUserDetails(response);
     });
 }
 
 function updateLastPush(lastDay) {
-    $(".gh-widget-active-time").append('Last active: ' + (lastDay ? lastDay + ' day(s) ago' : 'Today'));
+    appendToWidget(".gh-widget-active-time", "span", "", 'Last active: ' + (lastDay ? lastDay + ' day(s) ago' : 'Today'));
 }
 
 function lastPushedDay(repos) {
@@ -41,23 +75,19 @@ function lastPushedDay(repos) {
 }
 
 function updateUserDetails(user) {
-    var personDetails = $(".gh-widget-personal-details");
-    var widgetStats = $(".gh-widget-stats");
 
-    personDetails.append('<div class="full-name">' + user.name + '</div>');
+    appendToWidget(".gh-widget-personal-details", "div", "full-name", user.name);
     if (user.bio) {
-        personDetails.append('<div class="bio">' + user.bio + '</div>')
+        appendToWidget(".gh-widget-personal-details", "div", "bio", user.bio);
     }
     if (user.location) {
-        personDetails.append('<div class="location">&#9906; ' + user.location + '</div>');
+        appendToWidget(".gh-widget-personal-details", "div", "location", '&#9906; ' + user.location);
     }
-
-    widgetStats.append(' <div class="gh-widget-item"><div class="count">' + user.followers + '</div><div class="stat-name">followers</div></div>');
-    widgetStats.append(' <div class="gh-widget-item"><div class="count">' + user.following + '</div><div class="stat-name">following</div></div>');
-    widgetStats.append(' <div class="gh-widget-item"><div class="count">' + user.public_repos + '</div><div class="stat-name">repositories</div></div>');
-
-    $(".gh-widget-photo").append('<img src="' + user.avatar_url + '">');
-    $(".gh-widget-follow").append('<button><a class="gh-widget-link" target="new" href="' + user.html_url + '">Follow</a></button>');
+    appendToWidget(".gh-widget-stats", "div", "gh-widget-item", '<div class="count">' + user.followers + '</div><div class="stat-name">Followers</div>');
+    appendToWidget(".gh-widget-stats", "div", "gh-widget-item", '<div class="count">' + user.following + '</div><div class="stat-name">Following</div>');
+    appendToWidget(".gh-widget-stats", "div", "gh-widget-item", '<div class="count">' + user.public_repos + '</div><div class="stat-name">Repositories</div>');
+    appendToWidget(".gh-widget-photo", "span", "", '<img src="' + user.avatar_url + '">');
+    appendToWidget(".gh-widget-follow", "button", "", '<a class="gh-widget-link" target="new" href="' + user.html_url + '">Follow</a>')
 
 }
 
@@ -65,7 +95,7 @@ function updateRepoDetails(repos) {
 
     for (var i = 0; i < repos.length; i++) {
         var language = repos[i].language ? repos[i].language : "Unknown";
-        $(".gh-widget-repositories").append('<div class="gh-widget-container"><div class="gh-widget-item names"><div><a class="gh-widget-link" href="' + repos[i].repoUrl + '">' + repos[i].name + '</a></div></div><div class="gh-widget-item language"><div>' + language + '</div></div><div class="gh-widget-item stars"><div>&#9733;' + repos[i].stars + '</div></div></div>');
+        appendToWidget(".gh-widget-repositories", "div", "gh-widget-container", '<div class="gh-widget-item names"><div><a class="gh-widget-link" href="' + repos[i].repoUrl + '">' + repos[i].name + '</a></div></div><div class="gh-widget-item language"><div>' + language + '</div></div><div class="gh-widget-item stars"><div>&#9733;' + repos[i].stars + '</div></div>');
     }
 
 }
