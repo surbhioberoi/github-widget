436609b150bdd0282ff7c871f877728b234f7fc1
Surbhi Oberoi <siyaoberoi93@gmail.com>
2016-08-01 22:24:54 +0530

    adding support for multiple widgets in single page

--- src/widget.js
+++ src/widget.js
@@ -32,35 +32,42 @@ function ready(fn) {
 
 function start() {
     appendToWidget("body", "style", "", "@import url(https://fonts.googleapis.com/css?family=Noto+Sans:400,700);.gh-widget-link,.gh-widget-link:hover{text-decoration:none}.gh-widget-container{display:flex;flex-direction:row;flex-wrap:no-wrap;align-items:center;justify-content:center;color:#333;font-family:'Noto Sans',sans-serif}.gh-widget-personal-details .bio,.gh-widget-stats .count{color:#4078C0}.github-widget{border:1px solid #DDD;max-width:350px}.gh-widget-item{flex:1;text-align:center;padding:10px}.gh-widget-repositories .language{text-align:left}.gh-widget-repositories .language div,.gh-widget-repositories .stars div{padding:5px 0}.gh-widget-photo{flex:2}.gh-widget-photo img{border-radius:100%;max-width:90px}.gh-widget-personal-details{flex:6}.gh-widget-personal-details .full-name{font-size:1.5em;line-height:1.5em}.gh-widget-personal-details .location{font-size:.8em}.gh-widget-stats .count{font-size:1.2em;font-weight:700}.gh-widget-repositories .names{flex:2;text-align:left}.gh-widget-repositories .names div{padding:5px 0;text-overflow:ellipsis}.gh-widget-follow{flex:2}.gh-widget-active-time{flex:4;font-size:.8em}.gh-widget-heading{font-weight:400;color:#666}.gh-widget-hr{border:1px solid #DDD}.gh-widget-link{color:#4078C0}.gh-widget-follow button{width:100%;height:2em;border:none;background:#ddd}");
-    appendToWidget(".github-widget", "div", "", '<div class="gh-widget-container"><div class="gh-widget-item gh-widget-photo"></div><div class="gh-widget-item gh-widget-personal-details"></div></div><div class="gh-widget-container gh-widget-stats"></div><hr class="gh-widget-hr"><div class="gh-widget-container"><div class="gh-widget-item gh-widget-heading">Top repositories</div></div><div class="gh-widget-repositories"></div><div class="gh-widget-container"><div class="gh-widget-item gh-widget-follow"></div><div class="gh-widget-item gh-widget-active-time"></div></div>')
-    var username = document.querySelector(".github-widget").dataset.username;
-    fetchRepos(username);
-    fetchUserDetails(username);
+    var widgets = document.querySelectorAll('.github-widget');
+    for (var i = 0; i < widgets.length; i++) {
+        var parentNode = widgets[i];
+        parentNode.setAttribute("id", "widget" + i);
+        appendToWidget("#widget" + i, "div", "", '<div class="gh-widget-container"><div class="gh-widget-item gh-widget-photo"></div><div class="gh-widget-item gh-widget-personal-details"></div></div><div class="gh-widget-container gh-widget-stats"></div><hr class="gh-widget-hr"><div class="gh-widget-container"><div class="gh-widget-item gh-widget-heading">Top repositories</div></div><div class="gh-widget-repositories"></div><div class="gh-widget-container"><div class="gh-widget-item gh-widget-follow"></div><div class="gh-widget-item gh-widget-active-time"></div></div>')
+
+        var username = parentNode.dataset.username;
+
+        fetchRepos(username, "#widget" + i);
+        fetchUserDetails(username, "#widget" + i);
+    }
 }
 
 
 ready(start);
 
 
-function fetchRepos(username) {
+function fetchRepos(username, widgetId) {
     var url = "https://api.github.com/users/" + username + "/repos";
     getJSON(url, function(response) {
-        updateRepoDetails(topRepos(response));
-        updateLastPush(lastPushedDay(response));
+        updateRepoDetails(topRepos(response), widgetId);
+        updateLastPush(lastPushedDay(response), widgetId);
     });
 }
 
 
-function fetchUserDetails(username) {
+function fetchUserDetails(username, widgetId) {
     var url = "https://api.github.com/users/" + username;
     getJSON(url, function(response) {
-        updateUserDetails(response);
+        updateUserDetails(response, widgetId);
     });
 }
 
 
-function updateLastPush(lastDay) {
-    appendToWidget(".gh-widget-active-time", "span", "", 'Last active: ' + (lastDay ? lastDay + ' day(s) ago' : 'Today'));
+function updateLastPush(lastDay, widgetId) {
+    appendToWidget(widgetId + " .gh-widget-active-time", "span", "", 'Last active: ' + (lastDay ? lastDay + ' day(s) ago' : 'Today'));
 }
 
 
@@ -79,28 +86,28 @@ function lastPushedDay(repos) {
 }
 
 
-function updateUserDetails(user) {
+function updateUserDetails(user, widgetId) {
 
-    appendToWidget(".gh-widget-personal-details", "div", "full-name", user.name);
+    appendToWidget(widgetId + " .gh-widget-personal-details", "div", "full-name", user.name);
     if (user.bio) {
-        appendToWidget(".gh-widget-personal-details", "div", "bio", user.bio);
+        appendToWidget(widgetId + " .gh-widget-personal-details", "div", "bio", user.bio);
     }
     if (user.location) {
-        appendToWidget(".gh-widget-personal-details", "div", "location", '&#9906; ' + user.location);
+        appendToWidget(widgetId + " .gh-widget-personal-details", "div", "location", '&#9906; ' + user.location);
     }
-    appendToWidget(".gh-widget-stats", "div", "gh-widget-item", '<div class="count">' + user.followers + '</div><div class="stat-name">Followers</div>');
-    appendToWidget(".gh-widget-stats", "div", "gh-widget-item", '<div class="count">' + user.following + '</div><div class="stat-name">Following</div>');
-    appendToWidget(".gh-widget-stats", "div", "gh-widget-item", '<div class="count">' + user.public_repos + '</div><div class="stat-name">Repositories</div>');
-    appendToWidget(".gh-widget-photo", "span", "", '<img src="' + user.avatar_url + '">');
-    appendToWidget(".gh-widget-follow", "button", "", '<a class="gh-widget-link" target="new" href="' + user.html_url + '">Follow</a>')
+    appendToWidget(widgetId + " .gh-widget-stats", "div", "gh-widget-item", '<div class="count">' + user.followers + '</div><div class="stat-name">Followers</div>');
+    appendToWidget(widgetId + " .gh-widget-stats", "div", "gh-widget-item", '<div class="count">' + user.following + '</div><div class="stat-name">Following</div>');
+    appendToWidget(widgetId + " .gh-widget-stats", "div", "gh-widget-item", '<div class="count">' + user.public_repos + '</div><div class="stat-name">Repositories</div>');
+    appendToWidget(widgetId + " .gh-widget-photo", "span", "", '<img src="' + user.avatar_url + '">');
+    appendToWidget(widgetId + " .gh-widget-follow", "button", "", '<a class="gh-widget-link" target="new" href="' + user.html_url + '">Follow</a>')
 
 }
 
-function updateRepoDetails(repos) {
+function updateRepoDetails(repos, widgetId) {
 
     for (var i = 0; i < repos.length; i++) {
         var language = repos[i].language ? repos[i].language : "Unknown";
-        appendToWidget(".gh-widget-repositories", "div", "gh-widget-container", '<div class="gh-widget-item names"><div><a class="gh-widget-link" href="' + repos[i].repoUrl + '">' + repos[i].name + '</a></div></div><div class="gh-widget-item language"><div>' + language + '</div></div><div class="gh-widget-item stars"><div>&#9733;' + repos[i].stars + '</div></div>');
+        appendToWidget(widgetId + " .gh-widget-repositories", "div", "gh-widget-container", '<div class="gh-widget-item names"><div><a class="gh-widget-link" href="' + repos[i].repoUrl + '">' + repos[i].name + '</a></div></div><div class="gh-widget-item language"><div>' + language + '</div></div><div class="gh-widget-item stars"><div>&#9733;' + repos[i].stars + '</div></div>');
     }
 
 }
